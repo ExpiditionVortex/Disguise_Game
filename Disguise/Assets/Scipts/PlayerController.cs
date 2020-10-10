0bf75a7ac8ebd97681a3156038251016aa3fbe5f
Spencer Wilson <spencerewilson1@gmail.com>
2020-10-09 18:55:24 -0600

    Importing Unity Project + Player Movement Done

---
+++ Disguise/Assets/Scipts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * 
+ * Author: Spencer Wilson
+ * Description: Contains controls for player movement and abilities.
+ * 
+ */ 
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector2 newVelocity = new Vector2(x, y) * speed;
+
+        rb.velocity = newVelocity;
+    }
+}
